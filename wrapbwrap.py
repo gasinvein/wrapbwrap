e92f3481227065d1b3dba12c922aa03f85a0cea7
gasinvein <gasinvein@gmail.com>
2018-08-13 20:39:28 +0300

    Slightly more secure devices

--- wrapbwrap.py
+++ wrapbwrap.py
@@ -3,6 +3,7 @@ import subprocess
 import os
 import sys
 import argparse
+import glob
 
 
 bwrap_args = [
@@ -15,13 +16,14 @@ bwrap_args = [
     # TODO replace with something secure
     '--ro-bind', '/etc', '/etc',
     '--proc', '/proc',
-    '--dev-bind', '/dev', '/dev',
+    '--dev', '/dev',
     '--bind', '/tmp/.X11-unix', '/tmp/.X11-unix',
     '--ro-bind', os.environ['XAUTHORITY'], os.environ['XAUTHORITY'],
     '--bind', os.path.join(os.environ['XDG_RUNTIME_DIR'], 'pulse', 'native'), os.path.join(os.environ['XDG_RUNTIME_DIR'], 'pulse', 'native'),
     '--tmpfs', '/var',
     '--symlink', '../run', '/var/run',
-    '--tmpfs', os.environ['HOME']
+    '--tmpfs', os.environ['HOME'],
+    '--new-session',
 ]
 
 
@@ -42,6 +44,8 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--mount', action='append', type=str, help='Mount directory')
     parser.add_argument('--system-bus', action='store_true', help='Allow D-Bus system bus access (warning: insecure)')
     parser.add_argument('--session-bus', action='store_true', help='Allow D-Bus session bus access (warning: insecure)')
+    parser.add_argument('-i', '--input', action='store_true', help='Allow access to input devices')
+    parser.add_argument('--no-gpu', dest='gpu', action='store_false', help='Disallow access to GPU')
     parser.add_argument('cmd', type=str, nargs='+', help='Command to run')
     args = parser.parse_args()
 
@@ -65,12 +69,33 @@ if __name__ == '__main__':
             '--bind', dbus_session_socket, dbus_session_socket
         ]
 
+    if args.input:
+        bwrap_args += [
+            '--dev-bind', '/dev/uinput', '/dev/uinput',
+            '--dev-bind', '/dev/input', '/dev/input',
+        ]
+        for h in glob.glob('/dev/hidraw*'):
+            bwrap_args += [
+                '--dev-bind', h, h,
+            ]
+
+    if args.gpu:
+        bwrap_args += [
+            '--dev-bind', '/dev/dri', '/dev/dri',
+        ]
+        for n in glob.glob('/dev/nvidia*'):
+            bwrap_args += [
+                '--dev-bind', n, n,
+            ]
+
     cwd = os.getcwd()
 
     if is_kinda_safe_path(cwd):
         bwrap_args += ['--bind', cwd, cwd]
     else:
         print('Working directory {} includes sensitive directories, not mounting'.format(cwd), file=sys.stderr)
+
+    # TODO if command is a file and it's not inside cwd, mount it
 
     proc = subprocess.run(['bwrap'] + bwrap_args + ['--'] + args.cmd, shell=False, stdin=sys.stdin)
     sys.exit(proc.returncode)
