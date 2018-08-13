59b6ce47cee3537ecc63f343759129499bfdbf80
gasinvein <gasinvein@gmail.com>
2018-08-13 16:00:36 +0300

    Add mount and d-bus options

--- wrapbwrap.py
+++ wrapbwrap.py
@@ -20,6 +20,7 @@ bwrap_args = [
     '--bind', os.environ['XAUTHORITY'], os.environ['XAUTHORITY'],
     '--bind', os.path.join(os.environ['XDG_RUNTIME_DIR'], 'pulse', 'native'), os.path.join(os.environ['XDG_RUNTIME_DIR'], 'pulse', 'native'),
     '--tmpfs', '/var',
+    '--symlink', '../run', '/var/run',
     '--tmpfs', os.environ['HOME']
 ]
 
@@ -38,10 +39,31 @@ def is_kinda_safe_path(path):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run command in bwrap sandbox')
+    parser.add_argument('-m', '--mount', action='append', type=str, help='Mount directory')
+    parser.add_argument('--system-bus', action='store_true', help='Allow D-Bus system bus access (warning: insecure)')
+    parser.add_argument('--session-bus', action='store_true', help='Allow D-Bus session bus access (warning: insecure)')
     parser.add_argument('cmd', type=str, nargs='+', help='Command to run')
     args = parser.parse_args()
+
     if args.cmd[0].startswith('-'):
         raise ValueError('First argument must be command name')
+
+    if args.mount:
+        for m in args.mount:
+            mpath = os.path.abspath(m)
+            bwrap_args += ['--bind', mpath, mpath]
+
+    if args.system_bus:
+        dbus_system_socket = '/run/dbus/system_bus_socket'
+        bwrap_args += [
+            '--bind', dbus_system_socket, dbus_system_socket,
+        ]
+
+    if args.session_bus:
+        dbus_session_socket = os.environ['DBUS_SESSION_BUS_ADDRESS'].split('=')[1]
+        bwrap_args += [
+            '--bind', dbus_session_socket, dbus_session_socket
+        ]
 
     cwd = os.getcwd()
     if is_kinda_safe_path(cwd):
@@ -49,5 +71,5 @@ if __name__ == '__main__':
     else:
         print('Working directory {} includes sensitive directories, not mounting'.format(cwd), file=sys.stderr)
 
-    proc = subprocess.run(['bwrap'] + bwrap_args + args.cmd, stdin=sys.stdin)
+    proc = subprocess.run(['bwrap'] + bwrap_args + ['--'] + args.cmd, shell=False, stdin=sys.stdin)
     sys.exit(proc.returncode)
