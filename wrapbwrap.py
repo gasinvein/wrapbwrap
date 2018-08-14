c8f097d6dbfa1c6c1e9fd3bf605cfddb67d25ecb
gasinvein <gasinvein@gmail.com>
2018-08-14 13:51:49 +0300

    Initial wrapper class

--- wrapbwrap.py
+++ wrapbwrap.py
@@ -6,38 +6,78 @@ import argparse
 import glob
 
 
-bwrap_args = [
-    '--unshare-pid',
-    '--unshare-user-try',
-    '--ro-bind', '/usr', '/usr',
-    '--symlink', 'usr/lib', '/lib',
-    '--symlink', 'usr/lib64', '/lib64',
-    '--symlink', 'usr/bin', '/bin',
-    # TODO replace with something secure
-    '--ro-bind', '/etc', '/etc',
-    '--proc', '/proc',
-    '--dev', '/dev',
-    '--bind', '/tmp/.X11-unix', '/tmp/.X11-unix',
-    '--ro-bind', os.environ['XAUTHORITY'], os.environ['XAUTHORITY'],
-    '--bind', os.path.join(os.environ['XDG_RUNTIME_DIR'], 'pulse', 'native'), os.path.join(os.environ['XDG_RUNTIME_DIR'], 'pulse', 'native'),
-    '--tmpfs', '/var',
-    '--symlink', '../run', '/var/run',
-    '--tmpfs', os.environ['HOME'],
-    '--new-session',
-]
-
-
 def is_kinda_safe_path(path):
     """
     Check if given path is not one of the "sensitive" paths or its parents
     """
-    sensitive_paths = [os.environ['HOME'], '/home', '/var/lib', '/tmp']
+    sensitive_paths = [os.environ['HOME'], '/home', '/var/lib', '/tmp', '/run', '/run/user']
     given_path = os.path.realpath(path)
     for sp in sensitive_paths:
         if os.path.commonpath([given_path, sp]) == given_path:
             return False
     return True
 
+
+class BWrapper(object):
+    """
+    Object handling arguments for bwrap instance to be executed
+    """
+    _bwrap = 'bwrap'
+    _args = [
+        '--unshare-pid',
+        '--unshare-user-try',
+        '--ro-bind', '/usr', '/usr',
+        '--symlink', 'usr/lib', '/lib',
+        '--symlink', 'usr/lib64', '/lib64',
+        '--symlink', 'usr/bin', '/bin',
+        # TODO replace with something secure
+        '--ro-bind', '/etc', '/etc',
+        '--proc', '/proc',
+        '--dev', '/dev',
+        '--tmpfs', '/var',
+        '--symlink', '../run', '/var/run',
+#        '--new-session',
+    ]
+
+    def __init__(self, add_essentials=True):
+        if add_essentials:
+            self.add_mount('/tmp/.X11-unix')
+            self.add_mount(os.environ['XAUTHORITY'], False)
+            self.add_mount(os.path.join(os.environ['XDG_RUNTIME_DIR'], 'pulse', 'native'))
+            self.add_dir(os.environ['HOME'])
+
+    def add_mount(self, path, writable=True, dev=False):
+        if dev:
+            a = '--dev-bind'
+        elif writable:
+            a = '--bind'
+        else:
+            a = '--ro-bind'
+        p = os.path.abspath(path)
+        self._args += [a, p, p]
+
+    def add_dir(self, path, tmpfs=True):
+        a = '--tmpfs' if tmpfs else '--dir'
+        self._args += [a, path]
+
+    def add_symlink(self, target, name):
+        self._args += ['--symlink', target, name]
+
+    def add_dev(self, devpath):
+        d = os.path.join('/dev', devpath)
+        self._args += ['--dev-bind', d, d]
+
+    def get_bwrap_cmdline(self, cmdline, workdir=None):
+        if workdir is None:
+            workdir = os.getcwd()
+        return [self._bwrap] + self._args + ['--chdir', workdir] + ['--'] + cmdline
+
+    def run(self, cmdline, workdir=None, stdin=None):
+        if stdin is None:
+            stdin = sys.stdin
+        c = self.get_bwrap_cmdline(cmdline, workdir)
+        # TODO pass stadin to subprocess, don't let python handle it for itself
+        return subprocess.run(c, shell=False, stdin=stdin)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run command in bwrap sandbox')
@@ -49,53 +89,41 @@ if __name__ == '__main__':
     parser.add_argument('cmd', type=str, nargs='+', help='Command to run')
     args = parser.parse_args()
 
-    if args.cmd[0].startswith('-'):
-        raise ValueError('First argument must be command name')
+    wrapper = BWrapper(add_essentials=True)
+
+#    if args.cmd[0].startswith('-'):
+#        raise ValueError('First argument must be command name')
 
     if args.mount:
         for m in args.mount:
-            mpath = os.path.abspath(m)
-            bwrap_args += ['--bind', mpath, mpath]
+            wrapper.add_mount(os.path.abspath(m))
 
     if args.system_bus:
-        dbus_system_socket = '/run/dbus/system_bus_socket'
-        bwrap_args += [
-            '--bind', dbus_system_socket, dbus_system_socket,
-        ]
+        wrapper.add_mount('/run/dbus/system_bus_socket')
 
     if args.session_bus:
-        dbus_session_socket = os.environ['DBUS_SESSION_BUS_ADDRESS'].split('=')[1]
-        bwrap_args += [
-            '--bind', dbus_session_socket, dbus_session_socket
-        ]
+        wrapper.add_mount(os.environ['DBUS_SESSION_BUS_ADDRESS'].split('=')[1])
 
     if args.input:
-        bwrap_args += [
-            '--dev-bind', '/dev/uinput', '/dev/uinput',
-            '--dev-bind', '/dev/input', '/dev/input',
-        ]
-        for h in glob.glob('/dev/hidraw*'):
-            bwrap_args += [
-                '--dev-bind', h, h,
-            ]
+        wrapper.add_mount('/dev/uinput', dev=True)
+        wrapper.add_mount('/dev/input', dev=True)
+        for h in glob.glob('/dev/hidraw*', dev=True):
+            wrapper.add_mount(h, dev=True)
 
     if args.gpu:
-        bwrap_args += [
-            '--dev-bind', '/dev/dri', '/dev/dri',
-        ]
+        wrapper.add_dev('dri')
         for n in glob.glob('/dev/nvidia*'):
-            bwrap_args += [
-                '--dev-bind', n, n,
-            ]
+            wrapper.add_mount(n, dev=True)
 
     cwd = os.getcwd()
 
     if is_kinda_safe_path(cwd):
-        bwrap_args += ['--bind', cwd, cwd]
+        wrapper.add_mount(cwd)
     else:
+        wrapper.add_dir(cwd, tmpfs=False)
         print('Working directory {} includes sensitive directories, not mounting'.format(cwd), file=sys.stderr)
 
     # TODO if command is a file and it's not inside cwd, mount it
 
-    proc = subprocess.run(['bwrap'] + bwrap_args + ['--'] + args.cmd, shell=False, stdin=sys.stdin)
+    proc = wrapper.run(args.cmd, workdir=cwd)
     sys.exit(proc.returncode)
